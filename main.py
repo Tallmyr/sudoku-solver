663f52d89e9b47f140f4cef2c668081823053c00
Simon Tallmyr <simon@tallmyr.se>
2021-03-20 21:22:07 +0000

    First fully working one!

--- main.py
+++ main.py
@@ -1,14 +1,54 @@
+from itertools import chain
+from time import sleep
+
+from blessings import Terminal
+
 board = [
-    [7, 8, 0, 4, 0, 0, 1, 2, 0],
-    [6, 0, 0, 0, 7, 5, 0, 0, 9],
-    [0, 0, 0, 6, 0, 1, 0, 7, 8],
-    [0, 0, 7, 0, 4, 0, 2, 6, 0],
-    [0, 0, 1, 0, 5, 0, 9, 3, 0],
-    [9, 0, 4, 0, 6, 0, 0, 0, 5],
-    [0, 7, 0, 3, 0, 0, 0, 1, 2],
-    [1, 2, 0, 0, 0, 7, 4, 0, 0],
-    [0, 4, 9, 2, 0, 6, 0, 0, 7],
+    [0, 7, 1, 6, 8, 4, 0, 0, 0],
+    [0, 4, 9, 7, 0, 0, 0, 0, 0],
+    [5, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 8, 0, 0, 0, 0, 5, 0, 4],
+    [0, 0, 0, 3, 0, 7, 0, 0, 0],
+    [2, 0, 3, 0, 0, 0, 0, 9, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 9],
+    [0, 0, 0, 0, 0, 3, 7, 2, 0],
+    [0, 0, 0, 4, 9, 8, 6, 1, 0],
 ]
+term = Terminal()
+
+
+def boardprinter(board, row, col, iter):
+    location = (row * 9) + col
+    line = chain(*board)
+    count = 0
+    count2 = 0
+    countl = 0
+    with term.location(0, term.height - 14):
+        print(term.normal + "Iterations: " + term.red + str(iter))
+        for y, x in enumerate(line):
+            if count2 == 9:
+                print()
+                count2 = 0
+                count = 0
+                countl += 1
+            if countl == 3:
+                print(term.normal + "------ ------- ------")
+                countl = 0
+            if count == 3:
+                print(term.normal + "|", end=" ")
+                count = 0
+            if y < location:
+                print(term.green + str(x), end=" ")
+            elif y == location:
+                print(term.blue + str(x), end=" ")
+            else:
+                if x == 0:
+                    print(term.red + " ", end=" ")
+                else:
+                    print(term.normal + str(x), end=" ")
+            count += 1
+            count2 += 1
+
 
 def check(i, r, c, board):
     # Check Row
@@ -16,9 +56,18 @@ def check(i, r, c, board):
         if board[r][col] == i:
             return False
 
-        # Check Col
-        for row in range(9):
-            if board[row][c] == i:
+    # Check Col
+    for row in range(9):
+        if board[row][c] == i:
+            return False
+
+    # Check Boxes
+    boxw = r - r % 3
+    boxh = c - c % 3
+
+    for w in range(boxw, boxw + 3):
+        for h in range(boxh, boxh + 3):
+            if board[w][h] == i:
                 return False
 
     return True
@@ -26,13 +75,15 @@ def check(i, r, c, board):
 
 def solver(board):
     solved = False
-    history = []
     row = 0
     col = 0
     x = 1
     backtrack = False
+    history = []
+    iter = 0
     while not solved:
         if board[row][col] == 0 or backtrack:
+            iter += 1
             for i in range(x, 10):
                 valid = check(i, row, col, board)
                 if valid == True:
@@ -56,6 +107,7 @@ def solver(board):
                 row += 1
             else:
                 col += 1
+        boardprinter(board, row, col, iter)
     return board
 
 
