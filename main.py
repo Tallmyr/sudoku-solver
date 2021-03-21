ef848605fac213e2e48cb277854ccef51ec37e78
Simon Tallmyr <simon@tallmyr.se>
2021-03-21 14:20:11 +0000

    Accept arguments on runtime

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 from sudoku.parser import parser
 from sudoku.solver import solver
+from sudoku.boardprinter import boardprinter
+
+import argparse
 
 if __name__ == "__main__":
-    
-    print(solver(parser("samples/s15c.txt"), True))
+
+    args = argparse.ArgumentParser(description="Commandline Sudoku Solver")
+    args.add_argument("file", help="Textfile containing sudoku data")
+    args.add_argument("-v", "-visual",  help="Visual solver", action="store_true", default=False)
+    args = args.parse_args()
+
+    solved, iter = solver(parser(args.file), args.v)
+    boardprinter(solved, iter=iter)
